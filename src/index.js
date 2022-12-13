5d2a8a18a12d52abb61824cccce9b6c61b47cfd9
Luis Zubia <soyzubia@gmail.com>
2022-12-13 12:08:41 -0600

    Import [lodash] library in our index.js script.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// Lodash, now imported by this script
+import _ from 'lodash';
+
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, use of .join() method
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
