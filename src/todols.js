163e2ad5eca816aff854c006da48597e63144c0a
Luis Zubia <soyzubia@gmail.com>
2022-12-15 15:43:32 -0600

    Add save() method to the collection for saving it to localStorage & the constructor now retrieves the tasks saved in the localStorage to load them when the page loads.

--- src/todols.js
+++ src/todols.js
@@ -1,16 +1,24 @@
 // Lodash, now imported by this script
 import _ from 'lodash';
+import Task from './task.js';
 
 class TodoLS {
   constructor() {
-    this.tasks = [];
+    let readTasks = JSON.parse(localStorage.getItem('todoData'));
+    readTasks ??= [];
+    this.tasks = readTasks.map((task) => new Task(task));
   }
 
   hasTasks = () => this.tasks.length > 0;
 
+  save = () => {
+    localStorage.setItem('todoData', JSON.stringify(this.tasks));
+  }
+
   addTask = (task) => {
     task.index = this.tasks.length;
     this.tasks.push(task);
+    this.save();
     return _.last(this.tasks);
   }
 
@@ -18,6 +26,8 @@ class TodoLS {
     const previousSize = this.tasks.length;
 
     this.tasks = this.tasks.filter(({ index }) => index !== parseInt(i, 10));
+
+    this.save();
 
     return (previousSize !== this.tasks.length) ? parseInt(i, 10) : null;
   }
