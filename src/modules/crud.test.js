d6a0f3c01b43d12e49f4033ce5748ea1eb7e3bcb
Luis Zubia <soyzubia@gmail.com>
2022-12-23 15:14:24 -0600

    Fix linters errors.

--- src/modules/crud.test.js
+++ src/modules/crud.test.js
@@ -23,27 +23,20 @@ describe('Testing editing and updating funtion', () => {
   });
 
   test('Testing clear all completed task', () => {
-    console.log(todoList);
     const task1 = new Task('task 1');
     todoList.addTask(task1);
-    console.log(todoList);
     const task2 = new Task('task 2');
     todoList.addTask(task2);
-    console.log(todoList);
     const task3 = new Task('task 3');
     todoList.addTask(task3);
-    console.log(todoList);
     todoList.tasks[0].setCompleted({ completed: true });
     todoList.save();
     todoList.tasks[1].setCompleted({ completed: true });
     todoList.save();
     todoList.tasks[2].setCompleted({ completed: true });
     todoList.save();
-    console.log(todoList);
     const tasks = getAllCompleted();
-    console.log(tasks);
     deleteAllCompleted(tasks);
-    console.log(todoList);
     expect(todoList.tasks.length).toBe(0);
     todoList.clear();
   });
